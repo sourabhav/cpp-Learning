fe17b2de61e43c3aca2a675af060c9a9e30d8a12
Sourabha V <sourabha2107@gmail.com>
2026-08-11 20:35:38 +0530

    add: if program

---
+++ Control_Statements/if.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter value of a: ";
+    cin>>a;
+    cout<<"enter value of b: ";
+    cin>>b;
+    if (a>b){
+        cout<<a<<" is larger"<<"\n";
+    }else {
+        cout<<b<<" is larger"<<"\n";
+    }
+
+    return 0;
+}
