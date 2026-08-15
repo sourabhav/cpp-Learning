7e647ecd30a795cbe2f522b9102913a1b11692ec
Sourabha V <sourabha2107@gmail.com>
2026-08-15 12:57:17 +0530

    add: loop program

---
+++ Loops/loop1.cpp
@@ -0,0 +1,16 @@
+// FOR LOOP
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number:\n";
+    cin>>n;
+
+    for (int i=1;i<=n;i++)
+    {
+        cout<<"hello\n";
+    }
+    return 0;
+}
